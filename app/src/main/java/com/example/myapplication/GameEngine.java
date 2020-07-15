992e4bf5c1533178eebb10c108701b1774ca7270
Prashant More <pmore362@outlook.com>
2020-07-15 15:54:23 +0530

    test commit - 2

--- app/src/main/java/com/example/myapplication/GameEngine.java
+++ app/src/main/java/com/example/myapplication/GameEngine.java
@@ -1,15 +1,46 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GameEngine extends SurfaceView implements Runnable {
 
+    Canvas mCanvas;
+    SurfaceHolder mHolder;
+    Paint mPaint;
+
+    long mFps;
+
+    private int mScreenX;
+    private int mScreenY;
+
+    private long mHighScore;
+
+    private Thread mThread = null;
+    private boolean paused = true;
+    private volatile boolean mPlaying;
+
+
     public GameEngine(Context context, int x, int y){
+
         super(context);
+        mScreenX = x;
+        mScreenY = y;
+
+        mHolder = getHolder();
+        mPaint = new Paint();
+
+        startNewGame();
     }
     @Override
     public void run() {
+
+    }
+
+    void startNewGame(){
 
     }
 }
