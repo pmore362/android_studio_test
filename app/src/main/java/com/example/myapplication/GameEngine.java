52300fefe97820fb502c8d3aba55ca8306cc3e7f
Prashant <pmore362@outlook.com>
2020-07-23 19:18:28 +0530

    latest commit

--- app/src/main/java/com/example/myapplication/GameEngine.java
+++ app/src/main/java/com/example/myapplication/GameEngine.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -13,12 +15,13 @@ public class GameEngine extends SurfaceView implements Runnable {
     Paint mPaint;
 
     long mFps;
+    private final int MILLIS_IN_SECOND = 1000;
 
     private int mScreenX;
     private int mScreenY;
-
+    private int y = 0;
     private long mHighScore;
-
+    private int radius;
     private Thread mThread = null;
     private boolean paused = true;
     private volatile boolean mPlaying;
@@ -30,6 +33,8 @@ public class GameEngine extends SurfaceView implements Runnable {
         mScreenX = x;
         mScreenY = y;
 
+        radius = mScreenX / 8;
+
         mHolder = getHolder();
         mPaint = new Paint();
 
@@ -38,9 +43,55 @@ public class GameEngine extends SurfaceView implements Runnable {
     @Override
     public void run() {
 
+        while(mPlaying){
+            long frameTime = System.currentTimeMillis();
+
+            draw();
+            y = y+5;
+            Log.e("y", Integer.toString(y));
+
+            long timeThisFrame = System.currentTimeMillis() - frameTime;
+            if(timeThisFrame > 0){
+                mFps = MILLIS_IN_SECOND / timeThisFrame;
+            }
+        }
+
     }
 
     void startNewGame(){
 
+    }
+
+    void draw(){
+        if(mHolder.getSurface().isValid()){
+            mCanvas = mHolder.lockCanvas();
+            mCanvas.drawColor(Color.argb(255,26,128,152));
+
+            mPaint.setColor(Color.argb(255,255,255,255));
+            //mPaint.setShadowLayer(radius+5, mScreenX/2, y, Color.RED);
+
+            mCanvas.drawCircle(mScreenX/2, y, radius, mPaint);
+
+            //mPaint.setColor(Color.argb(255,255,255,255));
+
+
+            mHolder.unlockCanvasAndPost(mCanvas);
+        }
+    }
+
+    void pause(){
+        mPlaying = false;
+        try{
+            mThread.join();
+        }catch(InterruptedException e){
+            Log.e("Error", "JOining thread");
+        }
+    }
+
+    void resume(){
+        mPlaying = true;
+
+        mThread = new Thread(this);
+        mThread.start();
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    GameEngine mEngine;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        int height = metrics.heightPixels;
+        int width = metrics.widthPixels;
+
+        mEngine = new GameEngine(this, width, height);
+        setContentView(mEngine);
+
+        /*Log.v("Height", Integer.toString(height));
+        Log.v("Width", Integer.toString(width));
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point m_size = new Point();
+        display.getSize(m_size);
+        int m_width = m_size.x;
+        int m_height = m_size.y;
+        Log.v("My Width : ", Integer.toString(m_width));
+        Log.v("My Height : ", Integer.toString(m_height));*/
+
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mEngine.pause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mEngine.resume();
     }
 }
